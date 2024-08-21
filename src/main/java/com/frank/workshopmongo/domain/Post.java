9ccaa910749f2890e9aedbe6adbb1c9e8c4801dc
Frankwillians <fkffrannk02@gmail.com>
2024-08-20 21:14:18 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/frank/workshopmongo/domain/Post.java
+++ src/main/java/com/frank/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.frank.workshopmongo.domain;
 
 import com.frank.workshopmongo.dto.AuthorDTO;
+import com.frank.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post  implements Serializable {
@@ -17,6 +20,8 @@ public class Post  implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){}
 
@@ -68,6 +73,14 @@ public class Post  implements Serializable {
     public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -81,5 +94,6 @@ public class Post  implements Serializable {
     public int hashCode() {
         return id.hashCode();
     }
+
 
 }
