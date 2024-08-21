7a21a6259191467197db7b042a813a78e40d06ec
Frankwillians <fkffrannk02@gmail.com>
2024-08-20 21:06:01 -0300

    Obtendo post por id

---
+++ src/main/java/com/frank/workshopmongo/services/PostService.java
@@ -0,0 +1,33 @@
+package com.frank.workshopmongo.services;
+
+import com.frank.workshopmongo.domain.Post;
+import com.frank.workshopmongo.domain.User;
+import com.frank.workshopmongo.dto.UserDTO;
+import com.frank.workshopmongo.repository.PostRepository;
+import com.frank.workshopmongo.repository.UserRepository;
+import com.frank.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+
+
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+    }
+
+
+}
+
+
